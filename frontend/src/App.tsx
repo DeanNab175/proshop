c7ab062a11bd640ac8c0a95488f05da3ad4a909f
deannab175 <f.jscript@gmail.com>
2024-06-09 20:01:39 +0400

    Add header and footer component
    Add bootstrap, react-bootstrap and react-icons and add header and footer component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/header";
+import Footer from "./components/footer";
+
 function App() {
   return (
-    <div>Welcome to Proshop.</div>
-  )
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop.</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
